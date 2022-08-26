684cdbfc778c0239dc3a9fdd93f0320c80a8cb27
youssef sk <alsarakibiy@gmail.com>
2022-08-26 23:44:53 +0300

    added maping to location

--- main.js
+++ main.js
@@ -1,29 +1,19 @@
 import "./style.css";
-import { Map, View } from "ol";
-import TileLayer from "ol/layer/Tile";
-import OSM from "ol/source/OSM";
-
-new Map({
-  target: "map",
-  layers: [
-    new TileLayer({
-      source: new OSM(),
-    }),
-  ],
-  view: new View({
-    center: [0, 0],
-    zoom: 3,
-  }),
-});
 
 const searchBtn = document.querySelector("[data-btn]");
 const searchInput = document.querySelector("[data-input]");
+let mark = L.marker([1, 1]);
+const map = L.map("map");
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+  attribution: "© OpenStreetMap",
+}).addTo(map);
 const getIp = async (location = "") => {
   const ipRegex =
     /^(?:(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])(\.(?!$)|$)){4}$/;
 
   const respond = await fetch(
-    `https://geo.ipify.org/api/v2/country?apiKey=at_iixwNe0Fi0OMwlAguaaK4LkMAG5Se&${
+    `https://geo.ipify.org/api/v2/country,city?apiKey=at_iixwNe0Fi0OMwlAguaaK4LkMAG5Se&${
       ipRegex.test(location) ? " ipAddress=" : "domain="
     }${location}`
   );
@@ -34,21 +24,27 @@ const displayLocation = (
   ip = "Not found",
   isp = "Not found",
   timezone = "Not found",
-  region = "Not found"
+  region = "Not found",
+  latLng
 ) => {
   document.querySelector("[data-ip]").innerText = ip;
   document.querySelector("[data-location]").innerText = region;
   document.querySelector("[data-timezone]").innerText = timezone;
   document.querySelector("[data-ips]").innerText = isp;
+  mark.remove();
+  map.setView(latLng, 12);
+  mark = L.marker(latLng);
+  mark.addTo(map);
 };
 const getLocation = async () => {
   const locationData = await getIp(searchInput?.value);
   const {
     ip,
     isp,
-    location: { timezone, region },
+    location: { timezone, region, lat, lng },
   } = locationData;
-  displayLocation(ip, isp, timezone, region);
+  const latLng = [lat, lng];
+  displayLocation(ip, isp, timezone, region, latLng);
 };
 getLocation();
 searchBtn.addEventListener("click", getLocation);
